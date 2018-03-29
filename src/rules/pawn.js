fc20694051703c4fbd67d758ea35f5b9c25913cf
andr3w <liu.anray+github@gmail.com>
2018-03-28 22:04:45 -0400

    update

--- src/rules/pawn.js
+++ src/rules/pawn.js
@@ -1,7 +1,14 @@
-const getValidMoves = (gameState, selectedPiece) => {
-    return [];
+import Type from '../Type';
+import { get } from 'lodash';
+
+const getValidMoves = (piecesMatrix, selection) => {
+  const type = get(selection, 'piece.type');
+  if (type !== Type.PAWN) {
+    throw new Error(`Expected type ${Type.PAWN} but instead got ${type}.`);
+  }
+  return [];
 };
 
 export default {
-    getValidMoves,
+  getValidMoves,
 };
--- src/rules/pawn.test.js
+++ src/rules/pawn.test.js
@@ -1,7 +1,16 @@
 import pawn from './pawn';
+import Type from '../Type';
 
 describe('getValidMoves', () => {
-    it('returns valid moves', () => {
-        expect(pawn.getValidMoves()).toEqual([]);
+  it('returns an array', () => {
+    expect(pawn.getValidMoves([], {
+      piece: { type: Type.PAWN },
+    })).toEqual([]);
+  });
+
+  it('errors if selection is not a pawn', () => {
+    const validMoves = pawn.getValidMoves([], {
+      piece: { type: Type.KNIGHT }
     });
+  });
 });
