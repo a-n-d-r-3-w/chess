95ea2228d7968cfd2f18447768baea6790bcdd27
andr3w <liu.anray+github@gmail.com>
2018-03-31 21:21:29 -0400

    refactor

--- src/rules/pawn.js
+++ src/rules/pawn.js
@@ -1,18 +1,19 @@
 import Type from '../Type';
 import { get, isEmpty } from 'lodash';
 
-const getValidMoves = (piecesMatrix, selection) => {
+const getValidMoves = (pieces, selection) => {
   const type = get(selection, 'piece.type');
   if (type !== Type.PAWN) {
     throw new Error(`Expected type ${Type.PAWN} but instead got ${type}.`);
   }
   const { row, column } = selection.position;
   const validMoves = [];
-  if (!isEmpty(piecesMatrix[row + 1][column])) {
-    return [];
+  if (isEmpty(pieces[row + 1][column])) {
+    validMoves.push({ row: row + 1, column: column });
   }
-  validMoves.push({ row: row + 1, column: column });
-  if (row === 1) {
+  if (row === 1 &&
+    isEmpty(pieces[row + 1][column]) &&
+    isEmpty(pieces[row + 2][column])) {
     validMoves.push({ row: row + 2, column: column });
   }
   return validMoves;
--- src/rules/pawn.test.js
+++ src/rules/pawn.test.js
@@ -2,57 +2,42 @@ import pawn from './pawn';
 import Type from '../Type';
 import Piece from '../Piece';
 
-const NUM_COLS = 8;
+const EMPTY_ROW = (new Array(8)).fill({});
+const EMPTY_MATRIX = (new Array(8)).fill(EMPTY_ROW);
 
 describe('pawn', () => {
   it('can take 1 or 2 steps from its starting point', () => {
-    expect(pawn.getValidMoves([
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({})
-    ], {
+    expect(pawn.getValidMoves(EMPTY_MATRIX, {
       piece: { type: Type.PAWN },
-      position: { row: 1, column: 1 }
+      position: { row: 1, column: 0 }
     })).toEqual([
-      { row: 2, column: 1 },
-      { row: 3, column: 1 },
+      { row: 2, column: 0 },
+      { row: 3, column: 0 },
     ]);
   });
 
   it('can take only 1 step if it\'s not at its starting point', () => {
-    expect(pawn.getValidMoves([
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({})
-    ], {
+    expect(pawn.getValidMoves(EMPTY_MATRIX, {
       piece: { type: Type.PAWN },
-      position: { row: 3, column: 1 }
+      position: { row: 3, column: 0 }
     })).toEqual([
-      { row: 4, column: 1 },
+      { row: 4, column: 0 },
     ]);
   });
 
   it('cannot go forward if it is blocked by any piece', () => {
-    expect(pawn.getValidMoves([
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      [ Piece.WHITE_PAWN, {}, {}, {}, {}, {}, {}, {} ],
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({}),
-      (new Array(NUM_COLS)).fill({})
-    ], {
+    const pieces = EMPTY_MATRIX.slice();
+    pieces[2][0] = Piece.WHITE_PAWN;
+    expect(pawn.getValidMoves(pieces, {
+      piece: { type: Type.PAWN },
+      position: { row: 1, column: 0 }
+    })).toEqual([]);
+  });
+
+  it('can go diagonally to capture a piece of the opposite color', () => {
+    const pieces = EMPTY_MATRIX.slice();
+    pieces[2][1] = Piece.WHITE_PAWN;
+    expect(pawn.getValidMoves(pieces, {
       piece: { type: Type.PAWN },
       position: { row: 1, column: 0 }
     })).toEqual([]);
