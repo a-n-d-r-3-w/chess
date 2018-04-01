1a2987a621da01ad6c0c30fd7e2db7184c58457e
andr3w <liu.anray+github@gmail.com>
2018-03-31 20:26:02 -0400

    basic pawn movement

--- src/rules/pawn.js
+++ src/rules/pawn.js
@@ -6,7 +6,13 @@ const getValidMoves = (piecesMatrix, selection) => {
   if (type !== Type.PAWN) {
     throw new Error(`Expected type ${Type.PAWN} but instead got ${type}.`);
   }
-  return [];
+  const { row, column } = selection.position;
+  const validMoves = [];
+  validMoves.push({ row: row + 1, column: column });
+  if (row === 1) {
+    validMoves.push({ row: row + 2, column: column });
+  }
+  return validMoves;
 };
 
 export default {
--- src/rules/pawn.test.js
+++ src/rules/pawn.test.js
@@ -1,16 +1,26 @@
 import pawn from './pawn';
 import Type from '../Type';
+import Piece from '../Piece';
 
-describe('getValidMoves', () => {
-  it('returns an array', () => {
+const NUM_COLS = 8;
+
+describe('pawn', () => {
+  it('can take 1 or 2 steps from its starting point', () => {
     expect(pawn.getValidMoves([], {
       piece: { type: Type.PAWN },
-    })).toEqual([]);
+      position: { row: 1, column: 1 }
+    })).toEqual([
+      { row: 2, column: 1 },
+      { row: 3, column: 1 },
+    ]);
   });
 
-  it('errors if selection is not a pawn', () => {
-    const validMoves = pawn.getValidMoves([], {
-      piece: { type: Type.KNIGHT }
-    });
+  it('can take only 1 step if it\'s not at its starting point', () => {
+    expect(pawn.getValidMoves([], {
+      piece: { type: Type.PAWN },
+      position: { row: 3, column: 1 }
+    })).toEqual([
+      { row: 4, column: 1 },
+    ]);
   });
 });
