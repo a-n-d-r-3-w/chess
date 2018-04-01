216326f249a4c143b69d296d70554529852c8799
andr3w <liu.anray+github@gmail.com>
2018-03-31 22:37:22 -0400

    copy black pawn tests for white

--- src/rules/BlackPawnRules.test.js
+++ src/rules/BlackPawnRules.test.js
@@ -57,3 +57,53 @@ describe('black pawn', () => {
     })).toEqual([{ row: 2, column: 6 }]);
   });
 });
+
+describe('white pawn', () => {
+  it('can take 1 or 2 steps from its starting point', () => {
+    expect(pawn.getValidMoves(emptyMatrix, {
+      piece: Piece.WHITE_PAWN,
+      position: { row: 1, column: 0 }
+    })).toEqual([
+      { row: 2, column: 0 },
+      { row: 3, column: 0 },
+    ]);
+  });
+
+  it('can take only 1 step if it\'s not at its starting point', () => {
+    expect(pawn.getValidMoves(emptyMatrix, {
+      piece: Piece.WHITE_PAWN,
+      position: { row: 3, column: 0 }
+    })).toEqual([
+      { row: 4, column: 0 },
+    ]);
+  });
+
+  it('cannot go forward if it is blocked by any piece', () => {
+    const pieces = emptyMatrix.slice();
+    pieces[2][0] = Piece.WHITE_ROOK;
+    expect(pawn.getValidMoves(pieces, {
+      piece: Piece.WHITE_PAWN,
+      position: { row: 1, column: 0 }
+    })).toEqual([]);
+  });
+
+  it('can go diagonally right to capture a piece of the opposite color', () => {
+    const pieces = emptyMatrix.slice();
+    pieces[2][0] = Piece.WHITE_ROOK;
+    pieces[2][1] = Piece.BLACK_PAWN;
+    expect(pawn.getValidMoves(pieces, {
+      piece: Piece.WHITE_PAWN,
+      position: { row: 1, column: 0 }
+    })).toEqual([{ row: 2, column: 1 }]);
+  });
+
+  it('can go diagonally left to capture a piece of the opposite color', () => {
+    const pieces = emptyMatrix.slice();
+    pieces[2][7] = Piece.WHITE_ROOK;
+    pieces[2][6] = Piece.BLACK_PAWN;
+    expect(pawn.getValidMoves(pieces, {
+      piece: Piece.WHITE_PAWN,
+      position: { row: 1, column: 7 }
+    })).toEqual([{ row: 2, column: 6 }]);
+  });
+});
