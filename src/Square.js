de020827f8051e4d0cac8f788e3696bd54475282
andr3w <liu.anray+github@gmail.com>
2018-04-05 21:38:10 -0400

    refactor

--- src/Square.js
+++ src/Square.js
@@ -1,14 +1,14 @@
 export const isEmpty = square => square === null;
 
 export const isPieceOfSameColor = (square1, square2) => {
-    if (square1 === null || square2 === null) {
+    if (isEmpty(square1) || isEmpty(square2)) {
         return false;
     }
     return square1.color === square2.color;
 };
 
 export const isPieceOfDifferentColor = (square1, square2) => {
-    if (square1 === null || square2 === null) {
+    if (isEmpty(square1) || isEmpty(square2)) {
         return false;
     }
     return square1.color !== square2.color;
