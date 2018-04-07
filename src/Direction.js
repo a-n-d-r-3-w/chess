65f631d2dff2d5a23409c25666137f5ff1cdafa5
andr3w <liu.anray+github@gmail.com>
2018-04-06 22:21:00 -0400

    Direction

---
+++ src/Direction.js
@@ -0,0 +1,10 @@
+export default Object.freeze({
+    NORTH: 'north',
+    NORTHEAST: 'northeast',
+    EAST: 'east',
+    SOUTHEAST: 'southeast',
+    SOUTH: 'south',
+    SOUTHWEST: 'southwest',
+    WEST: 'west',
+    NORTHWEST: 'northwest'
+});
