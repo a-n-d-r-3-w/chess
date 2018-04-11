6b99c4dcbfe38d7545f3a3ccb040b13cb93e0496
andr3w <liu.anray+github@gmail.com>
2018-04-10 20:47:19 -0400

    fix pawn

--- src/rules/Pawn.js
+++ src/rules/Pawn.js
@@ -1,89 +1,87 @@
 import {get, isEmpty} from 'lodash';
-import {isBlack} from "../Color";
+import Color from '../Color';
 
-const isBlackPieceSelected = (selection) => isBlack(selection.piece.color);
-
-const canTakeAStepForward = (board, selection) => {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 1 : -1;
+const canTakeAStepForward = (board, {row, column}) => {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 1 : -1;
     return isEmpty(board.get({row: row + addend, column}));
 };
 
-const isAtStartingRow = (selection) => {
-    const STARTING_ROW = isBlackPieceSelected(selection) ? 1 : 6;
-    return selection.position.row === STARTING_ROW;
+const isAtStartingRow = (color, row) => {
+    const STARTING_ROW = color === Color.BLACK ? 1 : 6;
+    return row === STARTING_ROW;
 };
 
-const canTakeTwoStepsForward = (board, selection) => {
-    const {row, column} = selection.position;
-    const addend1 = isBlackPieceSelected(selection) ? 1 : -1;
-    const addend2 = isBlackPieceSelected(selection) ? 2 : -2;
-    return isAtStartingRow(selection) &&
+const canTakeTwoStepsForward = (board, {row, column}) => {
+    const piece = board.get({row, column});
+    const addend1 = piece.color === Color.BLACK ? 1 : -1;
+    const addend2 = piece.color === Color.BLACK ? 2 : -2;
+    return isAtStartingRow(piece.color, row) &&
         isEmpty(board.get({row: row + addend1, column})) &&
         isEmpty(board.get({row: row + addend2, column}));
 };
 
-const getOneStepForward = function (selection) {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 1 : -1;
+const getOneStepForward = function (board, {row, column}) {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 1 : -1;
     return {row: row + addend, column};
 };
 
-const getTwoStepsForward = function (selection) {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 2 : -2;
+const getTwoStepsForward = function (board, {row, column}) {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 2 : -2;
     return {row: row + addend, column};
 };
 
-const canCaptureEnemy1 = function (board, selection) {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 1 : -1;
+const canCaptureEnemy1 = function (board, {row, column}) {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 1 : -1;
     const target = board.get({row: row + addend, column: column + 1});
     if (target === null) {
         return false;
     }
-    return target.color !== selection.piece.color;
+    return target.color !== piece.color;
 };
 
-const canCaptureEnemy2 = function (board, selection) {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 1 : -1;
+const canCaptureEnemy2 = function (board, {row, column}) {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 1 : -1;
     const target = board.get({row: row + addend, column: column - 1});
     if (target === null) {
         return false;
     }
-    return target.color !== selection.piece.color;
+    return target.color !== piece.color;
 };
 
-const getEnemy1Position = function (selection) {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 1 : -1;
+const getEnemy1Position = function (board, {row, column}) {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 1 : -1;
     return {row: row + addend, column: column + 1};
 };
 
-const getEnemy2Position = function (selection) {
-    const {row, column} = selection.position;
-    const addend = isBlackPieceSelected(selection) ? 1 : -1;
+const getEnemy2Position = function (board, {row, column}) {
+    const piece = board.get({row, column});
+    const addend = piece.color === Color.BLACK ? 1 : -1;
     return {row: row + addend, column: column - 1};
 };
 
-const getMoves = (board, selection) => {
+const getMoves = (board, startingIndex) => {
     const moves = [];
 
-    if (canTakeAStepForward(board, selection)) {
-        moves.push(getOneStepForward(selection));
+    if (canTakeAStepForward(board, startingIndex)) {
+        moves.push(getOneStepForward(board, startingIndex));
     }
 
-    if (canTakeTwoStepsForward(board, selection)) {
-        moves.push(getTwoStepsForward(selection));
+    if (canTakeTwoStepsForward(board, startingIndex)) {
+        moves.push(getTwoStepsForward(board, startingIndex));
     }
 
-    if (canCaptureEnemy1(board, selection)) {
-        moves.push(getEnemy1Position(selection));
+    if (canCaptureEnemy1(board, startingIndex)) {
+        moves.push(getEnemy1Position(board, startingIndex));
     }
 
-    if (canCaptureEnemy2(board, selection)) {
-        moves.push(getEnemy2Position(selection));
+    if (canCaptureEnemy2(board, startingIndex)) {
+        moves.push(getEnemy2Position(board, startingIndex));
     }
 
     // TODO: Add support for en passant.
--- src/rules/Pawn.test.js
+++ src/rules/Pawn.test.js
@@ -10,94 +10,74 @@ beforeEach(() => {
 
 describe('black Pawn', () => {
     it('can take 1 or 2 steps from its starting point', () => {
-        expect(getPawnMoves(board, {
-            piece: Piece.BLACK_PAWN,
-            position: {row: 1, column: 0}
-        })).toEqual([
+        board.put(Piece.BLACK_PAWN, {row: 1, column: 0});
+        expect(getPawnMoves(board, {row: 1, column: 0})).toEqual([
             {row: 2, column: 0},
             {row: 3, column: 0},
         ]);
     });
 
     it('can take only 1 step if it\'s not at its starting point', () => {
-        expect(getPawnMoves(board, {
-            piece: Piece.BLACK_PAWN,
-            position: {row: 3, column: 0}
-        })).toEqual([
+        board.put(Piece.BLACK_PAWN, {row: 3, column: 0});
+        expect(getPawnMoves(board, {row: 3, column: 0})).toEqual([
             {row: 4, column: 0},
         ]);
     });
 
     it('cannot go forward if it is blocked by any piece', () => {
+        board.put(Piece.BLACK_PAWN, {row: 1, column: 0});
         board.put(Piece.BLACK_ROOK, {row: 2, column: 0});
-        expect(getPawnMoves(board, {
-            piece: Piece.BLACK_PAWN,
-            position: {row: 1, column: 0}
-        })).toEqual([]);
+        expect(getPawnMoves(board, {row: 1, column: 0})).toEqual([]);
     });
 
     it('can go diagonally right to capture a piece of the opposite color', () => {
+        board.put(Piece.BLACK_PAWN, {row: 1, column: 0});
         board.put(Piece.BLACK_ROOK, {row: 2, column: 0});
         board.put(Piece.WHITE_PAWN, {row: 2, column: 1});
-        expect(getPawnMoves(board, {
-            piece: Piece.BLACK_PAWN,
-            position: {row: 1, column: 0}
-        })).toEqual([{row: 2, column: 1}]);
+        expect(getPawnMoves(board, {row: 1, column: 0})).toEqual([{row: 2, column: 1}]);
     });
 
     it('can go diagonally left to capture a piece of the opposite color', () => {
+        board.put(Piece.BLACK_PAWN, {row: 1, column: 7});
         board.put(Piece.BLACK_ROOK, {row: 2, column: 7});
         board.put(Piece.WHITE_PAWN, {row: 2, column: 6});
-        expect(getPawnMoves(board, {
-            piece: Piece.BLACK_PAWN,
-            position: {row: 1, column: 7}
-        })).toEqual([{row: 2, column: 6}]);
+        expect(getPawnMoves(board, {row: 1, column: 7})).toEqual([{row: 2, column: 6}]);
     });
 });
 
 describe('white Pawn', () => {
     it('can take 1 or 2 steps from its starting point', () => {
-        expect(getPawnMoves(board, {
-            piece: Piece.WHITE_PAWN,
-            position: {row: 6, column: 0}
-        })).toEqual([
+        board.put(Piece.WHITE_PAWN, {row: 6, column: 0});
+        expect(getPawnMoves(board, {row: 6, column: 0})).toEqual([
             {row: 5, column: 0},
             {row: 4, column: 0},
         ]);
     });
 
     it('can take only 1 step if it\'s not at its starting point', () => {
-        expect(getPawnMoves(board, {
-            piece: Piece.WHITE_PAWN,
-            position: {row: 4, column: 0}
-        })).toEqual([
+        board.put(Piece.WHITE_PAWN, {row: 4, column: 0});
+        expect(getPawnMoves(board, {row: 4, column: 0})).toEqual([
             {row: 3, column: 0},
         ]);
     });
 
     it('cannot go forward if it is blocked by any piece', () => {
-        board.put(Piece.WHITE_ROOK, { row: 5, column: 0 });
-        expect(getPawnMoves(board, {
-            piece: Piece.WHITE_PAWN,
-            position: {row: 6, column: 0}
-        })).toEqual([]);
+        board.put(Piece.WHITE_PAWN, {row: 6, column: 0});
+        board.put(Piece.WHITE_ROOK, {row: 5, column: 0});
+        expect(getPawnMoves(board, {row: 6, column: 0})).toEqual([]);
     });
 
     it('can go diagonally right to capture a piece of the opposite color', () => {
-        board.put(Piece.WHITE_ROOK, { row: 5, column: 0 });
-        board.put(Piece.BLACK_PAWN, { row: 5, column: 1 });
-        expect(getPawnMoves(board, {
-            piece: Piece.WHITE_PAWN,
-            position: {row: 6, column: 0}
-        })).toEqual([{row: 5, column: 1}]);
+        board.put(Piece.WHITE_PAWN, {row: 6, column: 0});
+        board.put(Piece.WHITE_ROOK, {row: 5, column: 0});
+        board.put(Piece.BLACK_PAWN, {row: 5, column: 1});
+        expect(getPawnMoves(board, {row: 6, column: 0})).toEqual([{row: 5, column: 1}]);
     });
 
     it('can go diagonally left to capture a piece of the opposite color', () => {
-        board.put(Piece.WHITE_ROOK, { row: 5, column: 7 });
-        board.put(Piece.BLACK_PAWN, { row: 5, column: 6 });
-        expect(getPawnMoves(board, {
-            piece: Piece.WHITE_PAWN,
-            position: {row: 6, column: 7}
-        })).toEqual([{row: 5, column: 6}]);
+        board.put(Piece.WHITE_PAWN, {row: 6, column: 7});
+        board.put(Piece.WHITE_ROOK, {row: 5, column: 7});
+        board.put(Piece.BLACK_PAWN, {row: 5, column: 6});
+        expect(getPawnMoves(board, {row: 6, column: 7})).toEqual([{row: 5, column: 6}]);
     });
 });
